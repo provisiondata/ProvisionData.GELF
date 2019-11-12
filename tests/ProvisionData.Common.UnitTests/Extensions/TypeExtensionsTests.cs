c4d0afda79d4e2297ffa80371ffb2db9440b65b3
Doug Wilson <d@dkw.io>
2019-11-11 21:28:37 -0800

    Huge reorg

--- tests/ProvisionData.Common.UnitTests/Extensions/TypeExtensionsTests.cs
+++ tests/ProvisionData.Common.UnitTests/Extensions/TypeExtensionsTests.cs
@@ -23,11 +23,30 @@
  *
  *******************************************************************************/
 
-using System.Diagnostics.CodeAnalysis;
+namespace ProvisionData.Extensions
+{
+    using Shouldly;
+    using System;
+    using System.Linq;
+    using Xunit;
 
-[assembly: SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "It makes for pretier Build Definitions.")]
-[assembly: SuppressMessage("Design", "RCS1110:Declare type inside namespace.", Justification = "It makes for pretier Build Definitions.")]
-[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "It makes for pretier Build Definitions.")]
-[assembly: SuppressMessage("Readability", "RCS1018:Add default access modifier.", Justification = "It makes for pretier Build Definitions.")]
-[assembly: SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "It makes for pretier Build Definitions.")]
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "It makes for pretier Build Definitions.")]
+    public class TypeExtensionsTests
+    {
+        [Fact]
+        public void GetAllProperties_returns_inherited_properties()
+        {
+            TypeExtensions.GetAllProperties(typeof(Foo)).Count().ShouldBe(1);
+            TypeExtensions.GetAllProperties(typeof(Bar)).Count().ShouldBe(2);
+        }
+
+        private class Foo
+        {
+            public String Name { get; set; }
+        }
+
+        private class Bar : Foo
+        {
+            public Int32 Age { get; set; }
+        }
+    }
+}
