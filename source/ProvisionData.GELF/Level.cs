627b7d67cf888783136b1af8c844307c50920801
Doug Wilson <d@dkw.io>
2021-08-29 07:35:32 -0700

    Updated to Net 5.0, Added Entity Framework Core Auditing

--- source/ProvisionData.GELF/Level.cs
+++ source/ProvisionData.GELF/Level.cs
@@ -25,17 +25,7 @@
 
 namespace ProvisionData.GELF
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.IO;
-    using System.IO.Compression;
-    using System.Net.Http;
-    using System.Net.Sockets;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    public enum Level
+	public enum Level
     {
         Emergency = 0,
         Alert = 1,
